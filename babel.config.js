231c9a486e4da694516fcdf9d36ed886de9b2be7
Muhammad Kautsar Apriadi <kautsarady@gmail.com>
2022-07-02 00:05:36 +0700

    fix(babel): Add module resolver plugin, aliasing `src` import

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
-module.exports = function(api) {
+module.exports = function babelConfig(api) {
   api.cache(true);
+
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            src: './src',
+          },
+        },
+      ],
+    ],
   };
 };
